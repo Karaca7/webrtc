f48a3e7f4dea195df5d4fd21424c3b266136c322
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-26 14:29:57 +0300

    Create two different channels,send video and orthe
    Other data will pass through the "ortherdata" channel and is used to
    connect sockets and stream video or audio through the getroom channel.

---
+++ app.js
@@ -0,0 +1,47 @@
+const express = require("express");
+let app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "Views");
+
+app.use("/static", express.static("static"));
+///
+let http = require("http");
+
+let Serverapp = http.createServer(app);
+
+let { Server } = require("socket.io");
+///
+let io = new Server(Serverapp);
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+const { v4: uuidv4 } = require("uuid");
+app.get("/redirectToRoom", (req, res) => {
+  res.redirect(`/room/${uuidv4()}`);
+});
+app.get("/room/:id", (req, res) => {
+  res.render("room", { roomid: req.params.id });
+});
+
+//start socket father
+io.on("connection", (socket) => {
+  console.log("new socket fatheer");
+
+  socket.on("getroom", (roomid, userid) => {
+    // console.log("userid:" + userid);
+    // console.log("room:" + roomid);
+    socket.join(roomid);
+    io.sockets.in(roomid).emit("sendid", userid);
+  });
+
+  socket.on("ortherdata", (roomid, ortherdata) => {
+    console.log(ortherdata);
+    console.log("roomid:" + roomid);
+    io.sockets.in(roomid).emit("getortherdata", ortherdata);
+  });
+});
+
+Serverapp.listen(8080);
---
+++ static/clientsocket.js
@@ -0,0 +1,76 @@
+roomid = document.getElementById("roomid").innerText;
+const socket = io();
+var peer = new Peer();
+console.log("1");
+peer.on("open", (id) => {
+  socket.emit("getroom", roomid, id);
+});
+
+socket.on("sendid", (userid) => {
+  var getUserMedia =
+    navigator.getUserMedia ||
+    navigator.webkitGetUserMedia ||
+    navigator.mozGetUserMedia;
+  getUserMedia(
+    { video: true, audio: false },
+    function (stream) {
+      let localv = document.getElementById("myvideo");
+      addVideoStream(localv, stream);
+      var call = peer.call(userid, stream);
+      call.on("stream", function (remoteStream) {
+        let video = document.createElement("video");
+
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
+
+var getUserMedia =
+  navigator.getUserMedia ||
+  navigator.webkitGetUserMedia ||
+  navigator.mozGetUserMedia;
+peer.on("call", function (call) {
+  //it is mee
+  getUserMedia(
+    { video: true, audio: false },
+    function (stream) {
+      call.answer(stream); // Answer the call with an A/V stream.
+      call.on("stream", function (remoteStream) {
+        let video = document.createElement("video");
+        let remotev = document.getElementById("remote");
+        addVideoStream(video, remoteStream);
+      });
+    },
+    function (err) {
+      console.log("Failed to get local stream", err);
+    }
+  );
+});
+
+console.log("2");
+videoGrid = document.getElementById("videoGrid");
+function addVideoStream(video, stream) {
+  video.srcObject = stream; // video objesini stream le bağladık.
+  video.addEventListener("loadedmetadata", () => {
+    // data yüklendiinde
+    // video yu başlat
+    video.play();
+  });
+  videoGrid.append(video); // Append video element to videoGrid
+}
+
+senddata = document.getElementById("senddata");
+textingdata = document.getElementById("textingdata");
+
+senddata.addEventListener("click", () => {
+  socket.emit("ortherdata", roomid, textingdata.value);
+  // console.log(textingdata.value);
+});
+
+socket.on("getortherdata", (data) => {
+  console.log(data);
+});
