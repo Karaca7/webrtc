b2f49f117821755a0a751ffffe3eabf25b0f9b24
Karaca7 <karacaylmaz2016@gmail.com>
2022-02-27 14:33:42 +0300

    Fix clientsocket:remove excess

--- static/clientsocket.js
+++ static/clientsocket.js
@@ -41,7 +41,6 @@ peer.on("call", function (call) {
       call.answer(stream); // Answer the call with an A/V stream.
       call.on("stream", function (remoteStream) {
         let video = document.createElement("video");
-        let remotev = document.getElementById("remote");
         addVideoStream(video, remoteStream);
       });
     },
